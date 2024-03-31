ed04f80151baf59c03d985046f1c7d8a424319bf
Mhmmadhilmy <149981853+Mhmmadhilmy@users.noreply.github.com>
2024-04-01 06:18:37 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,5 +1,5 @@
 #/usr/bin/python
-#import requests,random,json,time,sys,os,re
+import requests,random,json,time,sys,os,re
 # -----------------------------------------------------------
 # created By ./Hilmy
 # Update 14 Juny 2021 10:57
